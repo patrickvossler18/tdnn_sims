3bd7dd6e87d20a59598d67346f4f62ddf9b8eb8c
Patrick Vossler <patrick.vossler18@gmail.com>
2022-04-04 14:58:46 -0700

    add launch template and bash start up files

---
+++ make_launch_json.py
@@ -0,0 +1,37 @@
+import sys, json, base64
+import boto3
+
+ec2_client = boto3.client("ec2")
+ENCODING = "utf-8"
+
+for setting_num in ["2", "3"]:
+    for p in [3, 5, 10, 15, 20]:
+        # read in the launch template as a dictionary
+        with open(f"setting_{setting_num}_launch_template.json", "r") as fp:
+            setting_template = json.load(fp)
+
+        # read in the bash script
+        with open(f"setting_{setting_num}_startup.sh", "r") as fp:
+            setting_startup = fp.readlines()
+        # change DIM=
+        setting_startup[1] = f"DIM={p}\n"
+        setting_startup_bytes = bytes("".join(setting_startup), ENCODING)
+        setting_startup_b64 = base64.b64encode(setting_startup_bytes).decode(ENCODING)
+        setting_template["UserData"] = setting_startup_b64
+        # launch_info_fp = f"setting_{setting_num}_{p}_launch_info.json"
+        # with open(launch_info_fp, "w") as fp:
+        #     json.dump(setting_template, fp)
+        lt = ec2_client.create_launch_template(
+            LaunchTemplateName=f"setting_{setting_num}_{p}_template",
+            LaunchTemplateData=setting_template,
+        )
+
+        lt_specifics = {
+            "LaunchTemplateId": lt["LaunchTemplate"]["LaunchTemplateId"],
+            "Version": "default",
+        }
+
+        launchedInstances = ec2_client.run_instances(
+            MaxCount=1, MinCount=1, LaunchTemplate=lt_specifics
+        )
+        print(launchedInstances)
--- setting_2_startup.sh
+++ setting_2_startup.sh
@@ -8,11 +8,11 @@ PUB_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 # install aws cli
 sudo apt-get -y install awscli
 
-curl -o /home/ubuntu/setting_1.R https://raw.githubusercontent.com/patrickvossler18/tdnn/master/non_parametric_sims/setting_1/setting_1.R?token=GHSAT0AAAAAABNXRMZQA6UPHNPD6KTYQIFOYSQQC2Q
+curl -o /home/ubuntu/setting_1.R https://raw.githubusercontent.com/patrickvossler18/tdnn_sims/main/setting_1.R
 
-curl -o /home/ubuntu/setting_2.R https://raw.githubusercontent.com/patrickvossler18/tdnn/master/non_parametric_sims/setting_2/setting_2.R?token=GHSAT0AAAAAABNXRMZRYPTCHROAAO36FJNOYSQQDZQ
+curl -o /home/ubuntu/setting_2.R https://raw.githubusercontent.com/patrickvossler18/tdnn_sims/main/setting_2.R
 
-curl -o /home/ubuntu/setting_3.R https://raw.githubusercontent.com/patrickvossler18/tdnn/master/non_parametric_sims/setting_3/setting_3.R?token=GHSAT0AAAAAABNXRMZRVHZC3G2PIRCVPZSEYSQQEIQ
+curl -o /home/ubuntu/setting_3.R https://raw.githubusercontent.com/patrickvossler18/tdnn_sims/main/setting_3.R
 
 cd /home/ubuntu
 sudo -i -u ubuntu R -e "devtools::install_github('patrickvossler18/tdnn_package@rcppthread')"
